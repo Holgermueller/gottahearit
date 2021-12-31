de372806acc64e44499cb26f067ba36ab3e3eba7
Holger Mueller <9jholger@gmail.com>
2021-12-31 12:35:11 -0600

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <div id="quote-box">
-        <h2 id="text">quote goes here</h2>
+        <h2 className="jumbotron" id="text">
+          quote goes here
+        </h2>
         <h4 id="author">author goes here</h4>
         <button id="new-quote">Get Quote</button>
       </div>
