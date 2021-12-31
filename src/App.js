b06381ff8673123fd3115de006f3e68bf1e84018
Holger Mueller <9jholger@gmail.com>
2021-12-31 12:23:10 -0600

    began scaffold of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <h2 id="text">quote goes here</h2>
+        <h4 id="author">author goes here</h4>
+        <button id="new-quote">Get Quote</button>
+      </div>
     </div>
   );
 }
