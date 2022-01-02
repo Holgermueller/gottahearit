3449e0302213ea2416378f0306b9c98feffd08ec
Holger Mueller <9jholger@gmail.com>
2022-01-01 21:22:47 -0600

    some styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div id="quote-box">
+      <div id="quote-box" className="border border-primary rounded">
         <h2 className="jumbotron" id="text">
           quote goes here
         </h2>
         <h4 id="author">author goes here</h4>
-        <button id="new-quote">Get Quote</button>
+        <button type="submit" className="btn btn-primary btn-lg" id="new-quote">
+          Get Quote
+        </button>
       </div>
     </div>
   );
