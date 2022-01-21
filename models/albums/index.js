58af72bb4943b48199eda47fa91b38cb4f5601ac
Holger Mueller <9jholger@gmail.com>
2022-01-20 19:38:54 -0600

    worked on models

--- models/albums/index.js
+++ models/albums/index.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const albumSchema = new Schema({
-  title: { title: String, required: true },
-  artist: { name: String, required: true },
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
   media: { type: String, required: true },
+  heardIt: { type: Boolean, required: true },
+  rating: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("albumSchema", albumSchema);
