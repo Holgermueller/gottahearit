b68a9fbff7ea90bed2b10fab0af99c8018a23f16
Holger Mueller <9jholger@gmail.com>
2022-03-27 16:00:24 -0500

    worked on nav

--- client/src/nav/nav.js
+++ client/src/nav/nav.js
@@ -1,14 +1,18 @@
 import { Component } from "react";
-const navStyles = {
-  borderTop: "2px solid black",
-  borderBottom: "2px solid black",
-};
+import "./nav.css";
 
 export default class Nav extends Component {
   render() {
     return (
-      <section id="navigation" className="m-2" style={navStyles}>
-        <h5>Nav will go here...</h5>
+      <section id="navigation" className="m-2">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Log Out</a>
+          </li>
+        </ul>
       </section>
     );
   }
