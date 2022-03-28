b390aa331dae195b00ad298c756d3420b7602dbc
Holger Mueller <9jholger@gmail.com>
2022-03-28 11:33:58 -0500

    style form

--- client/src/logreg/LoginRegistration.js
+++ client/src/logreg/LoginRegistration.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./LoginRegistration.css";
 
 export default class LogReg extends Component {
   render() {
     return (
-      <section>
+      <section className="form-section">
         <h2>Log In</h2>
-        <form>
-          <input></input>
-          <input></input>
-          <input type="submit"></input>
-        </form>
-        <button>Register here -></button>
+        <Form className="form">
+          <Form.Group className="mb-3">
+            <Form.Control
+              className="input"
+              type="email"
+              placeholder="email"
+            ></Form.Control>
+            <Form.Control
+              className="input"
+              type="password"
+              placeholder="password"
+            ></Form.Control>
+            <Button type="submit">Submit</Button>
+          </Form.Group>
+        </Form>
+        <Button className="register-link">Register here</Button>
       </section>
     );
   }
