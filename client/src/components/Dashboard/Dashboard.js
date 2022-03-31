994645f8829d47be617f6337f5db128f353ae115
Holger Mueller <9jholger@gmail.com>
2022-03-30 21:22:14 -0500

    added buttons and input field for filters

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Dashboard.css";
 
-function Dashboard() {
+function Dashboard(props) {
+  const [state] = useState({
+    filterTerm: "",
+  });
+
   return (
     <section>
       <h2>Dashboard</h2>
-      <h4>Filters will go here</h4>
+
+      <div className="btn-group">
+        <Button>Buy It</Button>
+        <Button>Borrow It</Button>
+        <Button>Pass</Button>
+      </div>
+
+      <Form className="form">
+        <Form.Group className="mb-3">
+          <Form.Control
+            className="input"
+            type="text"
+            id="filterTerm"
+            value={state.filterTerm}
+            placeholder="Filter by name..."
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+
       <h3>A list will go here</h3>
     </section>
   );
