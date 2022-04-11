1828798dd8b126ba859df05f8d9445d40f8d8622
Holger Mueller <9jholger@gmail.com>
2022-04-11 10:52:19 -0500

    got data to console log

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -3,41 +3,47 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./LoginPage.css";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login(props) {
-  const [logInData, setLoginData] = useState({
-    email: "",
-    password: "",
-  });
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  // const [logInData, setLoginData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
 
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    e.persist();
-    setLoginData((logInData) => ({
-      ...logInData,
-      [e.target.id]: e.target.value,
-    }));
-  };
+  // const handleChange = (e) => {
+  //   e.persist();
+  //   setLoginData((logInData) => ({
+  //     ...logInData,
+  //     [e.target.id]: e.target.value,
+  //   }));
+  // };
 
-  const handleSubmit = (e) => {
-    if (e) {
-      e.preventDefault();
-    }
-  };
+  // const handleSubmit = (e) => {
+  //   if (e) {
+  //     e.preventDefault();
+  //   }
+  // };
 
   return (
     <section className="form-section">
       <h2>Log In</h2>
-      <Form className="form" onSubmit={handleSubmit}>
+      <Form className="form" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3">
           <Form.Control
             className="input"
             type="email"
             id="email"
             placeholder="Email"
-            value={logInData.email}
-            onChange={handleChange}
+            //value={logInData.email}
+            //onChange={handleChange}
+            {...register("email")}
           ></Form.Control>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
@@ -47,13 +53,12 @@ function Login(props) {
             type="password"
             id="password"
             placeholder="Password"
-            value={logInData.password}
-            onChange={handleChange}
+            //value={logInData.password}
+            // onChange={handleChange}
+            {...register("password")}
           ></Form.Control>
         </Form.Group>
-        <Button type="submit" onClick={() => handleSubmit}>
-          Submit
-        </Button>
+        <Button type="submit">Submit</Button>
       </Form>
       <div>
         <span>Don't have an account? </span>
