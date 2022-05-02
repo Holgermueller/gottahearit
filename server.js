a1cdf4292f3e45ca02cc3ab928b5312cbbd0f952
Holger Mueller <9jholger@gmail.com>
2022-05-02 10:10:47 -0500

    some work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const compression = require("compression");
 const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,19 @@ app.use(logger("dev"));
 
 app.use(cors());
 app.use(compression());
+
+const mongoDB = process.env.MONGO_URI;
+
+//const mongoDB = "mongodb://127.0.0.1/";
+
+mongoose.connect(mongoDB || "mongodb://127.0.0.1", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
 app.listen(PORT, () => {
   console.log("API server now listening on PORT " + PORT);
