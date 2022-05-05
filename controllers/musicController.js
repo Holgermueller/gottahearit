6d824d3736780bd93c80ae8ce79adee7096cf475
Holger Mueller <9jholger@gmail.com>
2022-05-05 17:00:26 -0500

    built some controllers

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,13 +1,27 @@
 const Music = require("../models/albums");
 
 module.exports = {
-  getAllMusic: (req, res) => {},
+  getAllMusic: (req, res) => {
+    Music.find(req.query)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 
-  addMusic: (req, res) => {},
+  addMusic: (req, res) => {
+    const newMusic = {};
+    Music.create(newMusic)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 
   findMusicById: (req, res) => {},
 
   editMusicInfo: (req, res) => {},
 
-  deleteMusic: (req, res) => {},
+  deleteMusic: (req, res) => {
+    Music.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
