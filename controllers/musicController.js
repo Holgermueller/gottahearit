162b811f3777d028536a1b1e925e39db617c607e
Holger Mueller <9jholger@gmail.com>
2022-05-08 16:35:04 -0500

    refactored db connection

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,32 +1,39 @@
+const asyncHandler = require("express-async-handler");
+
 const Music = require("../models/albums");
 
 //@Desc Get music
 //@route GET /api/music
 //@access Private
-const getMusic = (req, res) => {
+const getMusic = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get listening" });
-};
+});
 
 //@Desc Add music
 //@route POST /api/music
 //@access Private
-const addMusic = (req, res) => {
+const addMusic = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add something");
+  }
+
   res.status(200).json({ message: "added music" });
-};
+});
 
 //@Desc Update music
 //@route PUT /api/music/:id
 //@access Private
-const updateMusic = (req, res) => {
+const updateMusic = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `added music ${req.params.id}` });
-};
+});
 
 //@Desc Delete music
 //@route DELETE /api/music/:id
 //@access Private
-const deleteMusic = (req, res) => {
+const deleteMusic = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete music ${req.params.id}` });
-};
+});
 
 module.exports = {
   getMusic,
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const compression = require("compression");
-const path = require("path");
+const colors = require("colors");
 const logger = require("morgan");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,20 +19,7 @@ app.use(compression());
 
 app.use("/api/music", require("./routes/musicRoutes"));
 
-const mongoDB = process.env.MONGO_URI;
-
-mongoose.connect(mongoDB || "mongodb://127.0.0.1/myFirstDatabase", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.once("open", () => {
-  console.log("db connection!");
-});
-
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("API server listening on PORT " + PORT);
