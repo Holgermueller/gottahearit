6c3a6d97e18b010cc336cb5ff95be2a3bb1386aa
Holger Mueller <9jholger@gmail.com>
2022-05-11 21:29:32 -0500

    worked on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,27 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/users");
 
+//@desc Register new data
+//@route POST /api/users
+//@access Public
+const registerUser = (req, res) => {
+  res.json({ message: "register user" });
+};
+
+//@desc Authenticate a data
+//@route POST /api/users/login
+//@access Public
+const loginUser = (req, res) => {
+  res.json({ message: "login user" });
+};
+
+//@desc Get user data
+//@route GET /api/users/me
+//@access Public
+const getMe = (req, res) => {
+  res.json({ message: "display user data" });
+};
+
 module.exports = {
   registerUser,
   loginUser,
