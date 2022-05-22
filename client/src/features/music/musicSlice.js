a255f8e6bafffc06f7c9e2991d9649e6202f8aff
Holger Mueller <9jholger@gmail.com>
2022-05-21 22:54:44 -0500

    workedon music slice

--- client/src/features/music/musicSlice.js
+++ client/src/features/music/musicSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import musicService from "./musicService";
 
 const initialState = {
   music: [],
@@ -8,11 +9,59 @@ const initialState = {
   message: "",
 };
 
+export const addMusic = createAsyncThunk(
+  "music/add",
+  async (musicData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await musicService.addMusic(musicData, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const getMusic = createAsyncThunk(
+  "music/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await musicService.getMusic(token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const musicSlice = createSlice({
   name: "music",
   initialState,
   reducers: {
     reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(addMusic.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addMusic.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.music.push(action.payload);
+      })
+      .addCase(addMusic.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
   },
 });
 
